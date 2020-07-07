c9509e1442b6d7b7faafb7ab05f143ccb97315d5
Heather Peterson <hpeterson462@gmail.com>
2020-07-07 10:43:06 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// import functions and grab DOM elements
+const myButton = document.getElementById('button');
+// initialize state
+let jellybeans = 0;
+// set event listeners to update state and DOM
